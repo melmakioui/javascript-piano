5b6313ba3b5aecee458dd25d8b165b90b4d6cc88
Mohamed <91630250+melmakioui@users.noreply.github.com>
2022-11-06 17:29:14 +0100

    Funcio autoPlay en funcionament

--- js/piano.js
+++ js/piano.js
@@ -1,38 +1,36 @@
-function Tecla(so,sostingut){
+function Tecla(so, sostingut) {
     this.so = so;
     this.sostingut = sostingut;
 }
 
-function Piano(){
+function Piano() {
     this.tecles = [];
-    this.notes = ["do","do-sust","re","re-sust","mi","fa","fa-sust","sol","sol-sust","la","la-sust","si","do7"];
+    this.notes = ["do", "do-sust", "re", "re-sust", "mi", "fa", "fa-sust", "sol", "sol-sust", "la", "la-sust", "si", "do7"];
 
-    this.init = function(){
-        for (var nota of this.notes)
-        {
+    this.init = function () {
+        for (var nota of this.notes) {
             var audio = nota + ".mp3"
             var esSostingut = nota.includes("sust");
-            this.tecles.push( new Tecla(audio,esSostingut));
+            this.tecles.push(new Tecla(audio, esSostingut));
         }
         this.injectaAudio();
     }
 
-    this.injectaAudio = function(){
+    this.injectaAudio = function () {
         var tecles = document.querySelectorAll('.tecla');
-    
+
         var idx = 0;
-        for (var tecla of this.tecles)
-        {
+        for (var tecla of this.tecles) {
             var audio = document.createElement('audio');
-            audio.setAttribute('src',`/assets/audio/${tecla.so}`);
-            tecles[idx].addEventListener('click',this.play);
+            audio.setAttribute('src', `/assets/audio/${tecla.so}`);
+            tecles[idx].addEventListener('click', this.play);
             tecles[idx].audio = audio; // afageix una propietat audio per poder reproduirla a la funcio activa tecla
             tecles[idx].appendChild(audio);
             idx++;
         }
     }
 
-    this.play = function(evt){
+    this.play = function (evt) {
         evt.stopPropagation();
         var tecla = evt.currentTarget;
         var audio = tecla.audio;
@@ -40,11 +38,42 @@ function Piano(){
         audio.play();
     }
 
-    this.autoPlay = function(partitura,botoReproduir){
+    this.autoPlay = function (partitura) {
 
+        var tecles = this.preparaPartitura(partitura);
+
+        var idx = 0;
+        var autoReprodueix = setInterval(function () {
+
+            tecles[idx].click();
+
+            if (idx === tecles.length - 1)
+                clearInterval(autoReprodueix);
+            idx++;    
+        }, 1000)
+
+    }
+
+    this.preparaPartitura = function (partitura) {
+        var tecles = document.querySelectorAll('.tecla');
+        var notes = [];
+        var teclesPartitura = [];
+
+        for (var tecla of tecles)
+            notes.push(tecla.getAttribute("data-note"));
+
+        for (var nota of partitura) {
+            var idx = notes.indexOf(nota);
+            teclesPartitura.push(tecles[idx]);
+        }
+
+        console.log(teclesPartitura);
+        return teclesPartitura;
     }
 }
 
 var piano = new Piano();
 
 piano.init();
+
+piano.autoPlay(["DO", "RE", "SOL", "DO", "FA"]);
