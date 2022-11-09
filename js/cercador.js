2d8f9ec39382d5404e7e56c1940ceb485860be0d
Mohamed <91630250+melmakioui@users.noreply.github.com>
2022-11-09 19:52:17 +0100

    Cercador dinamic amb addCerca

--- js/cercador.js
+++ js/cercador.js
@@ -1,9 +1,9 @@
-function Partitura(titol,notes){
+function Partitura(titol, notes) {
     this.titol = titol;
     this.notes = notes;
 }
 
-function Nota(nota,sostingut){
+function Nota(nota, sostingut) {
     this.nota = nota;
     this.sostingut = sostingut;
 }
@@ -13,11 +13,26 @@ function Cercador(partitures) {
     this.cerca = [];
     this.piano = new Piano();
 
-    this.addCerca = function (nota) {
-        var esSostingut = nota.includes("#");
-        var novaNota = new Nota(nota, esSostingut);
-        this.cerca.push(novaNota);
+
+    this.cercaMelodies = function () {
+        var cercador = document.querySelector('#cercador');
+        cercador.addEventListener('input', this.addCerca)
     }
+
+    this.addCerca = function (evt) {
+        this.cerca = [];
+        var arrayNotes = evt.currentTarget.value.split(" ");
+
+        for (var nota of arrayNotes) {
+            if (nota.length < 2)
+                break;
+            nota = nota.toUpperCase();
+            var esSostingut = nota.includes("#");
+            var novaNota = new Nota(nota,esSostingut);
+            this.cerca.push(novaNota);
+        }
+        this.mostraResultats();
+    }.bind(this)
 
     this.cercaPartitura = function () {
         var i = 0;
@@ -42,14 +57,14 @@ function Cercador(partitures) {
         return resultats;
     }
 
-    this.mostraResultats = function(){
+    this.mostraResultats = function () {
         var aside = document.querySelector('.resultats');
+        aside.innerHTML = "";
         var resultats = this.cercaPartitura();
 
-        for (var resultat of resultats) 
-        {
+        for (var resultat of resultats) {
             var div = document.createElement('div');
-            div.setAttribute('class','reproduir');
+            div.setAttribute('class', 'reproduir');
             aside.appendChild(div);
 
             var p = document.createElement('p');
@@ -57,43 +72,35 @@ function Cercador(partitures) {
             div.appendChild(p);
 
             var boto = document.createElement('button');
-            boto.setAttribute('class','play-btn')
+            boto.setAttribute('class', 'play-btn')
             boto.innerHTML = `REPRODUIR CANÇO`;
-            boto.addEventListener('click',this.autoPlay);
+            boto.addEventListener('click', this.autoPlay);
             boto.notes = resultat.notes;
             div.appendChild(boto);
         }
     }
 
-    this.autoPlay = function(evt){
+    this.autoPlay = function (evt) {
         var boto = evt.currentTarget;
         var partitura = boto.notes;
-        this.piano.autoPlay(partitura,boto) //TODO se mezclan los intervals
-        
-        // piano.autoPlay(partitura,boto);
+        this.piano.autoPlay(partitura, boto);
     }.bind(this)
 }
 
 //Array de partitures per la cerca de melodies.
 var partitures = [];
-var laBalanguera = new Partitura("La Balanguera",["DO", "RE", "MI", "FA", "FA", "SOL", "SOL", "LA#", "LA#"]);
+var laBalanguera = new Partitura("La Balanguera", ["DO", "RE", "MI", "FA", "FA", "SOL", "SOL", "LA#", "LA#"]);
 var happyBirthday = new Partitura("Happy BirthDay", ["DO", "DO", "RE", "DO", "FA", "MI", "DO", "DO", "RE", "DO", "SOL", "FA"]);
+var happyBirthday = new Partitura("Prova", ["DO", "RE", "MI", "DO", "DO", "RE", "MI", "DO", "MI", "FA", "SOL", "FA"]);
+
 partitures.push(laBalanguera);
 partitures.push(happyBirthday);
 
-
-/* --PROCESS-- */
-//Cerca de la partitura
 var cercador = new Cercador(partitures);
-//Cerca
-cercador.addCerca("DO");
-cercador.addCerca("RE");
-cercador.addCerca("MI");
-//Genera Botons
-cercador.mostraResultats();
+cercador.cercaMelodies();
 
 
-    /*
+/*
     @@@@Explicacio de l'asgorisme
     1. Per a cada partitura mir l'array de notes si existeix un subconjunt de l'array que s'esta cercant.
     2. Variables "i","j" que seran els dos punters que aniran iterant cada element SIMULTANIAMENT.
@@ -104,3 +111,12 @@ cercador.mostraResultats();
     7. Variables "i","j" tornen a 0 per mirar si existeix un subconjunt de la següent partitura.
     8. Finalment es retorna l'array de resultats, ja iterat totes les partitures. 
    */
+
+
+/*
+    @@@@@Explicacio de la cerca
+    1. Cada vegada que escric dins l'input envia els valors a addCerca.
+    2. Dins addCerca buit l'array cerca, despres faig us  del metode "split" per que me separi els strings i me retorni l'array d'strings
+    3. Dins el for si l'string es menor que 2 (DO..RE) no el fic dins l'array de cerca
+    4. En cas de que sigui menor que 2 el fic dins l'array de cerca i despres en mostra els resultats
+*/
